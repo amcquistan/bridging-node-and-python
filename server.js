95944f3a6503d5a19138e3f2e0171923effd3000
Adam McQuistan <adam.mcquistan@licor.com>
2019-08-18 15:05:49 -0500

    more clean up

--- server.js
+++ server.js
@@ -34,22 +34,6 @@ app.get('/api/house-price-model', (req, res) => {
   .catch(err => res.err(err));
 });
 
-app.get('/api/home-price-predict', (req, res) => {
-  // const { firstName, lastName, phone } = req.body
-  // TODO: create contact
-});
-
-// app.delete('/api/contacts/:id', (req, res) => {
-//   const id = parseInt(req.params.id)
-//   // TODO: find and delete contact by id
-// });
-
-// app.put('/api/contacts/:id', (req, res) => {
-//   const id = parseInt(req.params.id)
-//   const { firstName, lastName, phone } = req.body
-//   // TODO: find and update contact by id
-// });
-
 app.listen(3000, () => {
   console.log('Server is up and running on port 3000');
 });
